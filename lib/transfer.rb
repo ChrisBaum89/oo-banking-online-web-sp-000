86a66734b5a00ccf5158e5169e58250715c1b517
Chris Baum <christopher.baum@valpo.edu>
2021-02-04 02:23:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,19 +13,12 @@ class Transfer
   end
 
   def valid?
-    #sender.status == "open" && receiver.status == "open"
     sender.valid? && receiver.valid?
   end
 
   def execute_transaction
     if (@status == "pending")
-<<<<<<< HEAD
-      #binding.pry
       if sender.balance >= @amount && @sender.status == "open" && @receiver.status == "open"
-=======
-      binding.pry
-      if sender.balance >= @amount
->>>>>>> 20eed4f12b9980d080617bf33a86462b1a0c4dfa
         @sender.balance -= @amount
         @receiver.balance += @amount
         @status = "complete"
@@ -34,7 +27,6 @@ class Transfer
         "Transaction rejected. Please check your account balance."
       end
     end
-<<<<<<< HEAD
   end
 
   def reverse_transfer
@@ -43,9 +35,6 @@ class Transfer
       @sender.balance += @amount
       @status = "reversed"
     end
-=======
-
->>>>>>> 20eed4f12b9980d080617bf33a86462b1a0c4dfa
   end
 
 end
